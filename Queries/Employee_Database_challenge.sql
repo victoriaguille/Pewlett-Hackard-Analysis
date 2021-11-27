5b69bc765aa24cd643e9e6513f7a2d3803b456df
victoriaguille <91439219+victoriaguille@users.noreply.github.com>
2021-11-27 16:07:16 -0500

    created list of possible mentors

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -35,3 +35,23 @@ GROUP BY ut.title
 ORDER BY "count" DESC;
 
 select * from retiring_titles;
+
+-- create a list of employees eligible to be mentors
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+e.first_name, 
+e.last_name, 
+e.birth_date,
+de.from_date,
+de.to_date,
+ti.title
+INTO mentorship_eligibilty
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01')
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+select * from mentorship_eligibilty;
