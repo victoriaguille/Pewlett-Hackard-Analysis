9e25c4a6c27a1142843a598cede868465293a202
victoriaguille <91439219+victoriaguille@users.noreply.github.com>
2021-11-27 14:32:51 -0500

    created more retirement csv

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -13,3 +13,25 @@ WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 order by e.emp_no;
 
 select * from retirement_titles;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+select * from unique_titles;
+
+-- create a list of the number of employees retiring by title
+SELECT COUNT(ut.title) AS "count",
+ut.title
+INTO retiring_titles
+FROM unique_titles AS ut
+GROUP BY ut.title
+ORDER BY "count" DESC;
+
+select * from retiring_titles;
